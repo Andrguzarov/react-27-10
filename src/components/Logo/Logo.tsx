b89c48f94fbbe0deab86e6b0a58dbbd4c053e0ed
Andrii Guzarov <andrey.guzarov@gmail.com>
2023-02-25 17:14:02 +0200

    add product image

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,6 +2,7 @@ import logo from 'assets/logo.svg'
 import Typography from '@mui/material/Typography'
 
 type Props = {}
+
 const Logo = (props: Props) => {
     return (
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
